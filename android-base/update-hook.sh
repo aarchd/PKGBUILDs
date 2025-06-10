fa30e6f53f629bfa1b0e52b8908566fbd5ca3831
Deepak Meena <notwho53@gmail.com>
2025-06-10 17:07:19 +0530

    android-base: use aarchd version
    also provide useradd version of android-base
    
    Signed-off-by: Deepak Meena <notwho53@gmail.com>

--- android-base/update-hook.sh
+++ android-base/update-hook.sh
@@ -2,3 +2,6 @@
 
 echo "Overwriting /etc/login.defs with android-base version..."
 cp -f /usr/share/android-base/login.defs /etc/login.defs
+
+echo "Overwriting /etc/default/useradd with android-base version"
+cp -f /usr/share/android-base/useradd /etc/default/useradd
